149676345a6b4cca6e3bbd4110dd434841348aac
Tch1b0 <Jpour3006@gmail.com>
2023-10-07 13:23:28 +0200

    fix cone rotation

--- src/main.ts
+++ src/main.ts
@@ -39,19 +39,37 @@ function renderFunc() {
 
 // -=[ Drawing ]=-
 
+const AXIS_LENGTH = 5;
+const AXIS_WIDTH = 0.05;
+
 // draw x direction
-drawBox(5, 0.05, 0.05);
+drawBox(AXIS_LENGTH, AXIS_WIDTH, AXIS_WIDTH);
 // draw y direction
-drawBox(0.05, 5, 0.05);
+drawBox(AXIS_WIDTH, AXIS_LENGTH, AXIS_WIDTH);
 // draw z direction
-drawBox(0.05, 0.05, 5);
+drawBox(AXIS_WIDTH, AXIS_WIDTH, AXIS_LENGTH);
+
+// 270 deg rotation
+const CONE_ROTATION = Math.PI * 1.5;
 
 // draw x direction arrow
-drawCone(new THREE.Vector3(2.5, 0, 0), new THREE.Vector3(0, 0, 1), 4.7);
+drawCone(
+    new THREE.Vector3(2.5, 0, 0),
+    new THREE.Vector3(0, 0, 1),
+    CONE_ROTATION
+);
 // draw y direction arrow
-drawCone(new THREE.Vector3(0, 2.5, 0), new THREE.Vector3(0, 1, 0), 4.7);
+drawCone(
+    new THREE.Vector3(0, 2.5, 0),
+    new THREE.Vector3(0, 1, 0),
+    CONE_ROTATION
+);
 // draw z direction arrow
-drawCone(new THREE.Vector3(0, 0, 2.5), new THREE.Vector3(1, 0, 0), -4.7);
+drawCone(
+    new THREE.Vector3(0, 0, 2.5),
+    new THREE.Vector3(1, 0, 0),
+    -CONE_ROTATION
+);
 
 animate();
 drawGrid();
